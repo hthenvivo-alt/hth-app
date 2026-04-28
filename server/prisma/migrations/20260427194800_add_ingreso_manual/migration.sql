f4d5935a0bb15c50bdd7d30ff4f44e1df6db94ed
HTH Productora <hthenvivo@gmail.com>
2026-04-28 16:02:44 -0300

    fix: remove existing columns from migration

--- server/prisma/migrations/20260427194800_add_ingreso_manual/migration.sql
+++ server/prisma/migrations/20260427194800_add_ingreso_manual/migration.sql
@@ -1,5 +1,3 @@
 ALTER TABLE "SimulacionEscenario" ADD COLUMN "ingresoManual" BOOLEAN NOT NULL DEFAULT false;
 ALTER TABLE "SimulacionEscenario" ADD COLUMN "ventaManualMonto" DECIMAL(15, 2);
 ALTER TABLE "SimulacionEscenario" ADD COLUMN "aforoManual" INTEGER;
-ALTER TABLE "Funcion" ADD COLUMN "acuerdoPorcentaje" DECIMAL(5, 2);
-ALTER TABLE "Funcion" ADD COLUMN "acuerdoSobre" TEXT DEFAULT 'Neta';
